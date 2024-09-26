b1fcb826d532c11880b873da13455979d82f9f7a
Kite Tasx <kitetasxpete@gmail.com>
2024-09-26 16:49:23 -0500

    Se crea el proyecto con los ejemplos de los operadores

---
+++ operadoresProject/OperadoresDeAsignacion.py
@@ -0,0 +1,31 @@
+#Ejemplos de operadores de asignacion
+
+var1 = 5
+var2 = 12
+var3 = 8
+var4 = 14
+var5 = 6
+var6 = 8
+
+var1 += 3 #vari1 = vari+3  Asignacion con suma
+print(f"Asignacion con suma: {var1}")
+
+var2 -= 3 #vari2 = var2-3  Asignacion con resta
+print(f"Asignacion con suma: {var2}")
+
+var3 *= 3 #vari3 = var3*3  Asignacion con multiplicacion
+print(f"Asignacion con suma: {var3}")
+
+
+var4 /= 3 #vari4 = var4+3  Asignacion con sum
+print(f"Asignacion con suma: {var4}")
+
+var5 %= 3 #vari5 = var5%3  Asignacion con residuo
+print(f"Asignacion con suma: {var5}")
+
+var6 //= 3 #vari6 = var6//3  Asignacion con DIVIVION ENTERA
+print(f"Asignacion con suma: {var6}")
+
+print(var1)
+
+
